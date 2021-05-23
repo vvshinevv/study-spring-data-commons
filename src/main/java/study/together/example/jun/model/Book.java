36a7445405a67d36c71a356ca36b4c485cafcf82
윤현준 <homelus@daum.net>
2021-05-23 23:36:13 +0900

    Creation 기능 추가

--- src/main/java/study/together/example/jun/model/Book.java
+++ src/main/java/study/together/example/jun/model/Book.java
@@ -8,15 +8,17 @@ import java.util.Objects;
 @Document
 public class Book {
 
-    public Book(String id, String name, int price) {
+    public Book(String id, String name, String desc, int price) {
         this.id = id;
         this.name = name;
+        this.desc = desc;
         this.price = price;
     }
 
     @Id
     private String id;
     private String name;
+    private String desc;
     private int price;
 
     public String getId() {
@@ -25,6 +27,10 @@ public class Book {
 
     public String getName() {
         return name;
+    }
+
+    public String getDesc() {
+        return desc;
     }
 
     public int getPrice() {
@@ -36,12 +42,12 @@ public class Book {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Book book = (Book) o;
-        return price == book.price && Objects.equals(id, book.id) && Objects.equals(name, book.name);
+        return price == book.price && Objects.equals(name, book.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, price);
+        return Objects.hash(name, price);
     }
 
     @Override
